615535b6ab5472bede75ef1267fd92309b16d34e
Cyriac <drevia59@gmail.com>
2024-03-25 11:19:53 +0100

    Initial commit

---
+++ src/test/java/com/openclassrooms/medilaboGateway/MedilaboGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.medilaboGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilaboGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
